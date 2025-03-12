2c248f1b62c8192564bc405d7b1a9f258dfc8f7c
EvanRicketts <ericketts00@gmail.com>
2025-03-12 14:21:37 -0400

    Added additional fields + import / export working.
    Fields are now more than just name, they extend to all params for a faculty member. Import / export feature also has been implemented and is working to csv and xlsx.

--- custom_addons/faculty_management/models/faculty_member.py
+++ custom_addons/faculty_management/models/faculty_member.py
@@ -4,5 +4,26 @@ class FacultyMember(models.Model):
     _name = "faculty.member"
     _description = "Faculty Member"
 
-    name = fields.Char()
+    name = fields.Char(string="Full Name", required=True)
+    employee_id = fields.Char(string="Employee ID", required=True, unique=True)
+    email = fields.Char(string="Email", required=True)
+    years_of_service = fields.Integer(string="Years of Service")
+    fte = fields.Float(string="FTE")
+    last_academic_leave = fields.Date(string="Last Academic Leave")
+    next_eligible_leave = fields.Date(string="Next Eligible Academic Leave")
+    negotiated_leave = fields.Text(string="Negotiated Leave")
+    grants_held = fields.Text(string="Past Grants Held")
+    current_grants = fields.Text(string="Current Grants")
+    courses_taught = fields.Text(string="Courses Taught")
+    courses_current_year = fields.Text(string="Courses This Year")
+    teaching_release = fields.Text(string="Teaching Release")
+
+    past_supervision_ba = fields.Text(string="Past BA Supervision")
+    past_supervision_ma = fields.Text(string="Past MA Supervision")
+    past_supervision_phd = fields.Text(string="Past PhD Supervision")
+    current_supervision = fields.Text(string="Current Supervision")
+
+    service_duties = fields.Text(string="Service Duties")
+    major_service_duties = fields.Text(string="Major Service Duties")
+    current_service = fields.Text(string="Current Service")
 
